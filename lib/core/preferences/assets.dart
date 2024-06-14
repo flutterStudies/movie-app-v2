383f2fbc839719eeea24fda2bd039477e4ad04ae
4212ganteng <azizmuslim051@gmail.com>
2024-06-14 10:27:01 +0700

    init

---
+++ lib/core/preferences/assets.dart
@@ -0,0 +1,21 @@
+class MainAsset {
+  MainAsset._();
+
+  static const path = 'assets/images';
+
+  static const splash = '$path/splash.png';
+  static const avatar1 = '$path/avatar1.png';
+  static const avatar2 = '$path/avatar2.png';
+  static const avatar3 = '$path/avatar3.png';
+  static const avatar4 = '$path/avatar4.png';
+  static const movie1 = '$path/movie1.png';
+  static const movie2 = '$path/movie2.png';
+  static const movie3 = '$path/movie3.png';
+  static const movie4 = '$path/movie4.png';
+  static const movie5 = '$path/movie5.png';
+  static const movie6 = '$path/movie6.png';
+  static const spidermanBaner = '$path/spiderman-baner.png';
+  static const spidermanFloating = '$path/spiderman-floating.png';
+  static const spiderman1 = '$path/spiderman1.png';
+  static const spiderman2 = '$path/spiderman2.png';
+}
---
+++ lib/features/main/main_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int indexPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    const pages = [];
+    return Scaffold(
+      body: pages[indexPage],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        ],
+      ),
+    );
+  }
+}
